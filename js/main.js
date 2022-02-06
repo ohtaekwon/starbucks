8177b2240b5363828b7e3ef9673ba98089934b75
ohtaekwon <ohtk1119@gmail.com>
2022-02-06 14:26:04 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click',function(){
-  // Logic..
-  searchInputEl.focus();
-});
-
-
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  // set은 무엇인가를 지정해준다. setAttribute는 html의 속성에서 무엇인가를 지정할떄 쓰는 메소드
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur',function(){
-  searchEl.classList.remove('focused');
-  // set은 무엇인가를 지정해준다. setAttribute는 html의 속성에서 무엇인가를 지정할떄 쓰는 메소드
-  searchInputEl.setAttribute('placeholder','');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -156,7 +135,5 @@ spyEls.forEach(function(spyEl){
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();  // 2022가 나옴
 
 
